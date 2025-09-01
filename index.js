ec88a556d48d5b62dfe567fe914120801b0a676c
Mikael Lönnbäck <mikkus@windowslive.com>
2025-09-01 10:36:53 +0300

    dist

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(morgan('tiny'))
 
 morgan.token('body', (req) => {
   return req.method === 'POST' ? JSON.stringify(req.body) : ''
